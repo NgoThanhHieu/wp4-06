b3a8ba58d0f9d6d3e6637c47660fb0b8e680f1a4
NgoThanhHieu <thanhhieu.ngo@student.ossp.cz>
2022-01-12 11:18:21 +0100

    komentáře v detailu příspěvku

--- web-project/app/Presenters/PostPresenter.php
+++ web-project/app/Presenters/PostPresenter.php
@@ -15,9 +15,46 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 
 	public function renderShow(int $postId): void
 	{
-		$this->template->post = $this->database
+		$post = $this->database
 			->table('posts')
 			->get($postId);
+		if (!$post) {
+			$this->error('Stránka nebyla nalezena');
+		}
+		$this->template->post = $post;
+		$this->template->comments = $post->related('comments')->order('created_at');
+
 	}
+
+protected function createComponentCommentForm(): Form
+{
+	$form = new Form; // means Nette\Application\UI\Form
+
+	$form->addText('name', 'Jméno:')
+		->setRequired();
+
+	$form->addEmail('email', 'E-mail:');
+
+	$form->addTextArea('content', 'Komentář:')
+		->setRequired();
+
+	$form->addSubmit('send', 'Publikovat komentář');
+	$form->onSuccess[] = [$this, 'commentFormSucceeded'];
+	return $form;
+}
+public function commentFormSucceeded(\stdClass $data): void
+{
+	$postId = $this->getParameter('postId');
+
+	$this->database->table('comments')->insert([
+		'post_id' => $postId,
+		'name' => $data->name,
+		'email' => $data->email,
+		'content' => $data->content,
+	]);
+
+	$this->flashMessage('Děkuji za komentář', 'success');
+	$this->redirect('this');
+}
 }
 ?>
