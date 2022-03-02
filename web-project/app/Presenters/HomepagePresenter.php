00ac5ed0b0454f8148dbf4c94116c62fd441857f
NgoThanhHieu <Thanhhieu.ngo@student.ossp.cz>
2022-03-02 11:30:32 +0100

    user

--- web-project/app/Presenters/HomepagePresenter.php
+++ web-project/app/Presenters/HomepagePresenter.php
@@ -5,6 +5,7 @@
 namespace App\Presenters;
 
 use App\Model\PostFacade;
+use App\Model\UserFacade;
 use Nette;
 
 
@@ -12,14 +13,18 @@ use Nette;
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 	private PostFacade $facade;
+	private UserFacade $userFacade;
 
-	public function __construct(PostFacade $facade)
+	public function __construct(PostFacade $facade, UserFacade $userFacade)
 	{
 		$this->facade = $facade;
+		$this->userFacade = $userFacade;
 	}
 
 	public function renderDefault(): void
 	{
+		$this->userFacade->add("Admin", "admin@ossp.cz" , "secret");
+
 		$this->template->posts = $this->facade
 			->getPublicArticles()
 			->limit(5);
