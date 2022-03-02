8ee119576c2a51c4c074e8d5286209d911fe8435
NgoThanhHieu <Thanhhieu.ngo@student.ossp.cz>
2022-03-02 15:56:51 +0100

    smolchange

--- web-project/app/Presenters/HomepagePresenter.php
+++ web-project/app/Presenters/HomepagePresenter.php
@@ -15,7 +15,7 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 	private PostFacade $facade;
 	private UserFacade $userFacade;
 
-	public function __construct(PostFacade $facade, UserFacade $userFacade)
+	public function __construct(PostFacade $facade, /*UserFacade $userFacade*/)
 	{
 		$this->facade = $facade;
 		$this->userFacade = $userFacade;
@@ -23,7 +23,7 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
 	public function renderDefault(): void
 	{
-		$this->userFacade->add("Admin", "admin@ossp.cz" , "secret");
+		#$this->userFacade->add("Admin", "admin@ossp.cz" , "secret");
 
 		$this->template->posts = $this->facade
 			->getPublicArticles()
